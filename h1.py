bfb414aebfb4792a246ed274bf3fa5a4b00096c7
barash1311 <barash1311@gmail.com>
2023-12-16 19:25:15 +0530

    Create h1.py

---
+++ h1.py
@@ -0,0 +1,14 @@
+class a:
+    def __init__(self,age,name):
+        self.age=age
+        self.name=name
+    def music():
+        print("hello world")  
+        me(a,b)
+    def me(a,b):
+        print("hello")
+newacc=a.music()
+
+#new=a.me(2,3)
+
+
